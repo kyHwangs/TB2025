b24ed49197a6e150179b4019be929382c2fd454e
kyHwangs <kyuyeong.hwang@gmail.com>
2025-09-12 03:02:07 +0900

    update initializing script

--- buildNinstall.sh
+++ buildNinstall.sh
@@ -6,3 +6,8 @@ cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../install 
 make -j4 install
 cd ..
+source envset.sh
+cd analysis
+./compile.sh make_ntuple 
+./compile.sh make_summary
+chmod +x run_makeResolution.sh
